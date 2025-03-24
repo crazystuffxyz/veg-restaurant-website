2182b9174111da2ab706c26288d6a4b178107a05
Crazystuffxyz <125907466+crazystuffxyz@users.noreply.github.com>
2025-03-24 18:31:56 -0400

    Create Footer.vue
    Make a footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer color="grey darken-3" app padless>
+    <v-col class="text-center white--text py-4">
+      &copy; {{ new Date().getFullYear() }} Green Bites. All Rights Reserved.
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
