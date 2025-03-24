b020383a74ac2b0a5c67af9bfae01eb21ec4cb9c
Crazystuffxyz <125907466+crazystuffxyz@users.noreply.github.com>
2025-03-24 18:28:16 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-
-// Import Vuetify and its styles
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-
-// Optionally, import font icons (Material Design Icons, etc.)
 import '@mdi/font/css/materialdesignicons.css'
-
-// Import Vue Meta
 import { createMetaManager } from 'vue-meta'
 
 const vuetify = createVuetify()
