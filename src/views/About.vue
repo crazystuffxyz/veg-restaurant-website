6b178d4173396919c875bae5c6df5bb16f82acda
Crazystuffxyz <125907466+crazystuffxyz@users.noreply.github.com>
2025-03-24 18:33:44 -0400

    Create About.vue
    A description of what our restaurant is all about

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h2 class="text-center my-6">About Us</h2>
+        <p class="subtitle-1">
+          At Green Bites, we bring you authentic, nutritious, and delectable vegetarian and vegan dishes. Rooted in a farm-to-table philosophy, we support local farms while ensuring that every plate is a piece of art. Our chefs are passionate artisans crafting meals that delight both the palate and the planet.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "About",
+  metaInfo: {
+    title: 'About Green Bites'
+  }
+}
+</script>
