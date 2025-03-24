51cfc954328c46137474f630e8436d8233460ded
Crazystuffxyz <125907466+crazystuffxyz@users.noreply.github.com>
2025-03-24 18:29:13 -0400

    Create App.vue
    The actual vue code

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <NavigationBar />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import NavigationBar from "./components/NavigationBar.vue"
+import Footer from "./components/Footer.vue"
+
+export default {
+  name: "App",
+  components: { NavigationBar, Footer }
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
